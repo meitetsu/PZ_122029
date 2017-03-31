1fbe1ee2c0fc77725f1ba9e39866e88d10dcc12e
meitetsu <meitetsu@vip.onet.pl>
2017-03-31 12:08:58 +0200

    pt. 24.03.2017

--- reuters_j.py
+++ reuters_j.py
@@ -2,3 +2,10 @@ import nltk
 
 nltkDownloader = Downloader()
 nltkDownloader.download()
+
+# Plan:
+#   1. Co pełni rolę x, y w przypadku tekstu?
+#   2.
+#   3. Zbadać zależność wyników dla SVM linear od liczby cech (features)
+#   4. Zbadać zależność wyników dla SVM RBF od liczby cech(features)
+#   5. Co moglibyśmy zrobić żeby zmniejszyć complexity?
